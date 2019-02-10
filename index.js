89fcabee20c0fae555760e0d47de2b7199f08b82
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-02-10 11:17:20 +0530

    :sparkles: Enabled cors

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
 
+
+// checkout https://www.npmjs.com/package/cors to configure headers as per your needs.
+const cors = require('cors')
+
 // Set up mongoose connection
 const mongoose = require('mongoose');
 let dev_db_url = 'mongodb://samcc:Samhith123@ds127535.mlab.com:27535/cc';
@@ -12,6 +16,8 @@ mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(cors()) //enabled cors
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
